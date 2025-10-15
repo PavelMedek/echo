545e2f4664940f82b1a781bf1bdf437f8b41242d
PavelMedek <medek41@gmail.com>
2025-10-15 06:55:40 +0200

    Vapi Plugin

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@workspace/ui/components/sonner";
 
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
@@ -25,7 +26,10 @@ export default function RootLayout({
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
         <ClerkProvider>
-          <Providers>{children}</Providers>
+          <Providers>
+            <Toaster />
+            {children}
+          </Providers>
         </ClerkProvider>
       </body>
     </html>
