bcde92755d1287a81d090231042b7238a98fc036
PavelMedek <medek41@gmail.com>
2025-10-20 10:04:14 +0200

    Contact Panel

---
+++ packages/backend/convex/private/contactSessions.ts
@@ -0,0 +1,47 @@
+import { ConvexError, v } from "convex/values";
+import { query } from "../_generated/server";
+
+export const getOneByConversationId = query({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "identity not found",
+      });
+    }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "organization id not found",
+      });
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: `conversation not found`,
+      });
+    }
+
+    if (conversation.organizationId !== orgId) {
+      throw new ConvexError({
+        code: "FORBIDDEN",
+        message: `conversation does not belong to your organization`,
+      });
+    }
+
+    const contactSession = await ctx.db.get(conversation.contactSessionId);
+
+    return contactSession;
+  },
+});
