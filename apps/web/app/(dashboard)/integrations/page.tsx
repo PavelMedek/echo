2649137a5f63ca00126af6a58fe85ee53c9d0670
PavelMedek <medek41@gmail.com>
2025-10-27 08:34:41 +0100

    Integrations UI

--- apps/web/app/(dashboard)/integrations/page.tsx
+++ apps/web/app/(dashboard)/integrations/page.tsx
@@ -1,7 +1,8 @@
+import { IntegrationsView } from "@/modules/integrations/ui/views/integrations-view";
 import React from "react";
 
 const Page = () => {
-  return <div>integrations</div>;
+  return <IntegrationsView />;
 };
 
 export default Page;
